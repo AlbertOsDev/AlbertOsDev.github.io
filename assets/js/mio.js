720e4c4bcffcfd23e0207f7ed57e4efa4514a475
AlbertOsDev <albertcog2@gmail.com>
2023-08-04 16:00:57 -0400

    El formulario de contacto ya esta terminado con enlace directo al whastapp

--- assets/js/mio.js
+++ assets/js/mio.js
@@ -5,8 +5,23 @@ document.querySelector('form').addEventListener('submit', e =>{
     )
     //alert(JSON.stringify(data))
     var wp = JSON.stringify(data)
-    alert(wp.indexOf('name'))
-    alert(wp[wp.indexOf('name')])
-    
-    
+   // alert(wp.indexOf('name'))
+    //alert(wp[wp.indexOf('name')])
+  
+ //   var enviar = document.getElementById("enviar").href
 })
+//document.getElementById("nombre").value="Noombreaaaaaaaaaaa"
+function saludo(){
+    var nombre = document.getElementById("nombre").value //Captura el input de nombre
+    var mensaje = document.getElementById("mensaje").value //Captura el textarea del mensaje
+    var enlace = "https://wa.me/59178752135?text=" +"*"+ nombre +"*"+ "%20" + mensaje
+    document.getElementById("enviar").href =enlace //se asigna el enlace href con el mensaje
+    var c = document.getElementById("enviar").href
+   // Agregar atributo htmll
+    // var href = document.querySelector('a')  
+    //href.setAttribute("href",enlace)
+    //Agregar atributo htmll
+   // var c = document.querySelector('a').href
+
+    alert(c)
+}
